672f1cd1a706a0147a67e75bcfbed118a33b98dd
Starcloudsea <starsea152@126.com>
2024-03-31 14:18:14 +0800

    修复评论系统不会自动刷新导致不同页面评论不对以及切换语言也会导致评论不对的问题，以及更换端口（我系统又抢我端口&@&~…%*%*#！#￥@(╯‵□′)╯︵┻━┻）

--- docs/.vitepress/theme/components/Commentaries/Commentaries.vue
+++ docs/.vitepress/theme/components/Commentaries/Commentaries.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="comment">
         <component
-            v-if="isDark"
+            v-if="refresh"
             :is="'script'"
             src="https://giscus.app/client.js"
             data-repo="Starcloudsea/StarseaDB"
@@ -13,37 +13,33 @@
             data-reactions-enabled="1"
             data-emit-metadata="0"
             data-input-position="top"
-            data-theme="dark_tritanopia"
+            :data-theme="isDark === true ? 'dark_tritanopia' : 'light_tritanopia'"
             :data-lang="lang === 'zh-CN' ? 'zh-CN' : 'en'"
             crossorigin="anonymous"
             async>
         </component>
-        <component
-            v-else
-            :is="'script'"
-            src="https://giscus.app/client.js"
-            data-repo="Starcloudsea/StarseaDB"
-            data-repo-id="R_kgDOLYAhKw"
-            data-category="Commentaries"
-            data-category-id="DIC_kwDOLYAhK84Cd6h4"
-            data-mapping="title"
-            data-strict="1"
-            data-reactions-enabled="1"
-            data-emit-metadata="0"
-            data-input-position="top"
-            data-theme="light_tritanopia"
-            :data-lang="lang === 'zh-CN' ? 'zh-CN' : 'en'"
-            crossorigin="anonymous"
-            async>
-        </component>
+
     </div>
 </template>
 
 <script setup lang="ts">
     import { useData } from 'vitepress';
+    import { nextTick, ref, watch } from 'vue'
+    import { useRoute } from 'vitepress'
     // 获取当前配色方案
     const { isDark } = useData();
     const { lang } = useData();
+
+
+    const route = useRoute()
+    const refresh = ref(true);
+
+    watch([route, lang, isDark], async () => {
+        refresh.value = false;
+        await nextTick();
+        refresh.value = true;
+    })
+
 </script>
 
 <style scoped>
